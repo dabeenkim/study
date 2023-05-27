0dc0f144c9e02c38535bf553d39d33c8a3f1c1c4
dabeenkim <124576278+dabeenkim@users.noreply.github.com>
2023-05-27 23:21:09 +0900

    [level 0] Title: 각도기, Time: 0.06 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120829. 각도기/각도기.js
@@ -0,0 +1,13 @@
+function solution(angle) {
+    var answer = 0
+    if(0 < angle && angle< 90){
+        return answer = 1
+    }if(angle === 90){
+        return answer = 2
+    }if(90 < angle && angle < 180){
+        return answer = 3
+    }if(angle === 180){
+        return answer = 4
+    }
+    return answer;
+}
